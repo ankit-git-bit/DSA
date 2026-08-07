fc792a6cb7e600e1684dabcc83af2f847936a060
Ankit <69582937+ankit-git-bit@users.noreply.github.com>
2026-08-07 17:12:08 +0530

    Time: 15 ms (44.49%) | Memory: 19.4 MB (75.75%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,3 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s)==sorted(t)
+        if len(s)!=len(t):
+            return False
+        count={}
+        for i in range(len(s)):
+            count[s[i]]=1+count.get(s[i],0)
+        for j in t:
+            if j not in count or count[j]==0:
+                return False
+            count[j]-=1
+        return True
