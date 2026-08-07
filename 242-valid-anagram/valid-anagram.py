e9753cf2c69a38e2fac592c90bbc5c5f3bc7eb38
Ankit <69582937+ankit-git-bit@users.noreply.github.com>
2026-08-07 16:55:04 +0530

    Time: 20 ms (6.21%) | Memory: 19.6 MB (26.32%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,11 +3,6 @@ class Solution:
         if len(s)!=len(t):
             return False
 
-        counter={}
-        for char in s:
-            counter[char]=counter.get(char,0)+1
-        for char in t:
-            if char not in counter or counter[char]==0:
-                return False
-            counter[char]-=1
-        return True
+        s_sort="".join(sorted(s))
+        t_sort="".join(sorted(t))
+        return s_sort==t_sort
